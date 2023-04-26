5a16e4a39b840c16ce63f6a22d4e9a19bc96b7dc
rob <robert_regan86@yahoo.com>
2023-04-26 07:28:33 -0400

    made responsive

--- frontend/src/components/PDFViewer.jsx
+++ frontend/src/components/PDFViewer.jsx
@@ -1,9 +1,24 @@
-// components/PDFViewer.js
-import React from 'react'
+// src/components/PDFViewer.js
+
+import React, { useState, useEffect } from 'react'
 import { Document, Page } from 'react-pdf'
 
 const PDFViewer = ({ pdfFile }) => {
-  const [numPages, setNumPages] = React.useState(null)
+  const [numPages, setNumPages] = useState(null)
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setWidth(window.innerWidth * 0.8)
+    }
+
+    updateWidth()
+    window.addEventListener('resize', updateWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateWidth)
+    }
+  }, [])
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
@@ -12,8 +27,12 @@ const PDFViewer = ({ pdfFile }) => {
   return (
     <div>
       <Document file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
-        {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            width={width}
+          />
         ))}
       </Document>
     </div>
